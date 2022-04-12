a69698fdacbb9b5ab21d93a86502f349cc58d175
unknown <jasoniyi>
2022-04-12 11:13:36 +0100

    refactor components

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,11 +1,9 @@
 import React, { useState, useContext, useEffect } from 'react'
-import check from '../images/icon-check.svg'
-import SelectList from '../utils/SelectList'
 import { GlobalContext } from '../context/GlobalState'
 
 
 const AddForm = () => {
-    const {todoList, addTodo } = useContext(GlobalContext)
+    const { todoList, addTodo } = useContext(GlobalContext)
     const [todo, setTodo ] = useState('')
 
     useEffect(() => {
@@ -16,13 +14,11 @@ const AddForm = () => {
       e.preventDefault()
       addTodo(todo)
       setTodo('')
-      console.log('add', todoList)
     }
 
 
   return (
     <div className="form-wrapper">
-        <SelectList />
         <div className="">
             <form onSubmit={handleSubmit}>
                 <input
